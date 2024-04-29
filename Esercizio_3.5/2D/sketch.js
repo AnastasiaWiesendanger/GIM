ffec0e17d77933de3eb149c81a25bfb131666e0a
AnastasiaWiesendanger <131657773+AnastasiaWiesendanger@users.noreply.github.com>
2024-04-29 14:29:00 +0200

    mod. code

--- Esercizio_3.5/2D/sketch.js
+++ Esercizio_3.5/2D/sketch.js
@@ -12,7 +12,7 @@ function draw(){     // questo fa già un loop    // + Velocità inizio dettata 
 
 
 for(let i=0; i<50; i=i+1) {  // 1 variabile, 2 condizione, 3 incremento valore a ogni ciclo
-
+                                  // quindi goccie appaiono tra le 0 e 50 a turno, sempre + 1
 	let spessore = random(1, 3)
     let lunghezza = random(0, 100)
     let posX = random(0, width)  // width è la larghezza del canvas
